7c67b173d6bcb9b05115eff13be5dd33d6e4fae3
ItsRealGaming <DestinyHero789@gmail.com>
2021-11-27 11:07:29 +0700

    Update SeperatorAttribute.cs

--- Runtime/Attributes/Drawers/SeperatorAttribute.cs
+++ Runtime/Attributes/Drawers/SeperatorAttribute.cs
@@ -8,8 +8,4 @@ public class SeperatorAttribute : AdditionDrawerAttribute
 
     public int Height { get; set; } = 1;
     public string ColorParameter { get; set; } = "#CDCDCD";
-
-    public SeperatorAttribute(float normalize) {
-        Normalize = Mathf.Clamp01(normalize);
-    }
 }
